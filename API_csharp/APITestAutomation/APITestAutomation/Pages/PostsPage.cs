7726bad8b7a4550384545529dcc406ba27bc3d56
Betania Jimenez Jimenez <bjimenez@agilisa.com>
2024-02-16 17:44:51 -0400

    API_SHARP

---
+++ API_csharp/APITestAutomation/APITestAutomation/Pages/PostsPage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Extensions;
+
+namespace APITestAutomation
+{
+    public class PostsPage
+    {
+        private readonly RestClient _client;
+       
+
+        public PostsPage()
+        {
+            //Endpoint del API de pruebas
+            _client = new RestClient("https://jsonplaceholder.typicode.com");
+        }
+
+        public RestResponse GetPosts()
+        {
+            //creamos un nuevo objeto RestRequest, que representa una solicitud HTTP
+            var request = new RestRequest("/posts", Method.Get);
+
+            //ejecutamos la solicitud HTTP utilizando el cliente RestSharp (_client) y pasamos el objeto RestRequest 
+            return _client.Execute(request); //El método Execute realiza la solicitud HTTP y devuelve un objeto RestResponse
+        }
+        //Metodo para enviar el request por el id
+        public RestResponse GetPostByID(string id)
+        {
+            var request = new RestRequest("/posts/"+id , Method.Get);
+            return _client.Execute(request);
+        }
+        //Metodo para resgistrar un nuevo post
+        public RestResponse PostNewPost(string title, string body, int userID)
+        {
+            var request = new RestRequest("/posts", Method.Post);
+            //como es un post se envian los campos del request
+            request.AddJsonBody(new { title, body, userID });
+            return _client.Execute(request);
+        }
+        //Metodo para Eliminar el post creado
+        public RestResponse DeleteNewPost(string idPost)
+        {
+            var request = new RestRequest("/posts/" + idPost, Method.Delete);
+            return _client.Execute(request);
+        }
+        public RestResponse PutPost(int idPost, string title, string body, int userId)
+        {
+            var request = new RestRequest($"/posts/{idPost.ToString()}" + idPost, Method.Put);
+            request.AddJsonBody(new { idPost, title, body, userId });
+            return _client.Execute(request);
+        }
+    }
+}
---
+++ API_csharp/APITestAutomation/APITestAutomation/Tests/PostsTests.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Newtonsoft.Json.Linq;
+
+namespace APITestAutomation.Tests
+{
+    [TestFixture]
+    public class PostsTests
+    {
+        private int _idpost; //para elimara el post creado
+        [Test]
+        public void GetPostsTest()
+        {
+            var postsPage = new PostsPage();
+            var response = postsPage.GetPosts();
+
+            // Agrega aquí las aserciones para verificar la respuesta
+            Console.WriteLine(response.Content);
+            Assert.That(response.IsSuccessful);
+
+           
+
+        }
+        [Test]
+        public void GetStatusCodeTest()
+        {
+            var postsPage = new PostsPage();
+            var response = postsPage.GetPosts();
+
+            // Agrega aquí las aserciones para verificar la respuesta
+            Console.WriteLine(response.Content);
+            Console.WriteLine(response.StatusCode);
+            // Verifica si la respuesta de la solicitud HTTP fue exitosa (código de estado 200)
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+
+
+        }
+        [Test]
+        public void GetPostByIdTest()
+        {
+            var postsPage = new PostsPage();
+            string id = "38";
+            var response = postsPage.GetPostByID(id);
+
+            // Agrega aquí las aserciones para verificar la respuesta
+            Console.WriteLine(response.Content);
+            Console.WriteLine(response.StatusCode);
+            //obteniendo los campos del response
+            var jsonResponse = JObject.Parse(response.Content);
+
+            int idPost = jsonResponse["id"].Value<int>();
+            Assert.AreEqual(int.Parse(id), idPost); //Validamos que sea el dato esperado
+
+
+        }
+
+        [Test]
+        public void PostNewTest()
+        {
+            var postsPage = new PostsPage();
+            string title = "QA REGRESION POST";
+            string body = "Esto es solo un ejercicio";
+            int userID = 12;
+
+            var response = postsPage.PostNewPost(title,body, userID);
+
+            
+            Console.WriteLine(response.Content);
+            Console.WriteLine(response.StatusCode);
+            //obteniendo los campos del response
+            var jsonResponse = JObject.Parse(response.Content);
+
+            string postTitle = jsonResponse["title"].Value<string>();
+            string postBody = jsonResponse["body"].Value<string>();
+            int userIdPost = jsonResponse["userID"].Value<int>();
+
+            this._idpost = jsonResponse["id"].Value<int>();
+
+            Assert.AreEqual(title, postTitle); //Validamos que sea el dato esperado
+            Assert.AreEqual(body, postBody);
+            Assert.AreEqual(userID, userIdPost);
+
+
+        }
+        [Test]
+        public void DeleteNewPostTest()
+        {
+            var postsPage = new PostsPage();
+
+            var response = postsPage.DeleteNewPost(this._idpost.ToString());
+
+
+            Console.WriteLine(response.Content);
+            Console.WriteLine(response.StatusCode);
+            //obteniendo los campos del response
+            var jsonResponse = JObject.Parse(response.Content);
+
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+
+
+
+        }
+        [Test]
+        public void PutPostTest()
+        {
+            var postsPage = new PostsPage();
+            string title = "QA REGRESION POST";
+            string body = "Esto es solo un ejercicio";
+            int userID = 12;
+            var response = postsPage.PutPost(this._idpost,title,body,userID);
+
+
+            Console.WriteLine(response.Content);
+            Console.WriteLine(response.StatusCode);
+            //obteniendo los campos del response
+            var jsonResponse = JObject.Parse(response.Content);
+
+
+            
+            string postTitle = jsonResponse["title"].Value<string>();
+            string postBody = jsonResponse["body"].Value<string>();
+            int userIdPost = jsonResponse["userID"].Value<int>();
+
+            this._idpost = jsonResponse["id"].Value<int>();
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, response.StatusCode);
+            Assert.AreEqual(title, postTitle); //Validamos que sea el dato esperado
+            Assert.AreEqual(body, postBody);
+            Assert.AreEqual(userID, userIdPost);
+
+
+        }
+
+
+    }
+}
